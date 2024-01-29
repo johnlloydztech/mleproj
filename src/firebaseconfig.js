a970239a24e4a57933340d7c5ff51915a53f20ac
John Lloyd Ponce <75487625+johnlloydztech@users.noreply.github.com>
2024-01-29 15:38:06 +0800

    Update firebaseconfig.js

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzqviSWQkRTPguJCJxU-QnszzRlKJNp-Q",
-    authDomain: "queuing-57020.firebaseapp.com",
-    databaseURL: "https://queuing-57020-default-rtdb.firebaseio.com",
-    projectId: "queuing-57020",
-    storageBucket: "queuing-57020.appspot.com",
-    messagingSenderId: "136468190970",
-    appId: "1:136468190970:web:3d6efa3a8ca79959f29b43",
-    measurementId: "G-K0NWVGM8D0"
+    apiKey: "AIzaSyBu-UAAaVFwq8Oq8dECn7zBFm-5wcbXMgU",
+      authDomain: "tryy-b404e.firebaseapp.com",
+      databaseURL: "https://tryy-b404e-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "tryy-b404e",
+      storageBucket: "tryy-b404e.appspot.com",
+      messagingSenderId: "421081701748",
+      appId: "1:421081701748:web:44fe5ff92f75cff3456021",
+      measurementId: "G-KNJWBFCNHH"
 };
 
 const app = initializeApp(firebaseConfig);
