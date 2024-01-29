f8f4a728db7136c185a948782ad339523ef63136
John Lloyd Ponce <75487625+johnlloydztech@users.noreply.github.com>
2024-01-29 14:37:19 +0800

    Update firebaseconfig.js

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAzqviSWQkRTPguJCJxU-QnszzRlKJNp-Q",
@@ -13,5 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export default app;
